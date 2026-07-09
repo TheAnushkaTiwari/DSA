1af08a1ebaaf37b2591c3348bdd2b147d22dc75d
TheAnushkaTiwari <anushkatiwari8197@gmail.com>
2026-07-09 22:29:44 +0530

    Time: 7 ms (63.10%) | Memory: 19.2 MB (52.86%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,8 +1,21 @@
+#without using string and with minimumspace complexity
+def is_pallindrome(x):
+    if x<0 or (x%10==0 and x!=0):
+        return False
+    head,tail=x,0
+    while head>tail:
+        tail= tail*10 + head%10
+        head= head//10
+    return head==tail or head==(tail//10)
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        return is_pallindrome(x)
+        ''' Using string
         num_string=str(x)
         reverse_num=num_string[::-1]
         if reverse_num==num_string:
             return True
         else:
             return False
+        '''
